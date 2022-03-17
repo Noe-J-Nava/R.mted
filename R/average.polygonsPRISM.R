d33f879fb1273fd49c7cd3e0a96e62cfd13bea03
Noe-J-Nava <97967131+Noe-J-Nava@users.noreply.github.com>
2022-03-17 12:18:42 -0500

    March 17, 22: :heavy_plus_sign: :zap: Improve the performance of average.polygonsPRISM() with more dependencies.

--- R/average.polygonsPRISM.R
+++ R/average.polygonsPRISM.R
@@ -1,37 +1,63 @@
 #' @title Calculate polygons average weather variable from PRISM raster
 #'
 #' @param varName Variable name is either 'ppt', 'tmax', or 'tmin'.
-#' @param day Day must be in format yyyymmdd (e.g., 199910113, for yyyy = 1991, mm = 10, and dd = 13).
+#' @param dayStart Initial day must be in format %Y/%m/%d  (e.g., 19991/01/13, for %Y = 1991, %m = 10, and %d = 13).
+#' @param dayEnd Last day must be in format %Y/%m/%d  (e.g., 19991/01/13, for %Y = 1991, %m = 10, and %d = 13).
 #' @param data_dir Directory where prism data is
 #' @param shpFile Polygons shapefile in "SpatialPolygonsDataFrame".
 #'
 #' @return Average mean of squared grid within the polygon for either variable.
 #' @import prism
+#' @import lubridate
+#' @importFrom stringr str_pad
 #' @importFrom exactextractr exact_extract
 #'
 #' @export
 #'
 #' @examples
-averge.polygonsPRISM <- function(varName, day, data_dir, shpFile) {
+averge.polygonsPRISM <- function(varName, dayStart, dayEnd, data_dir, shpFile) {
 
-  # Robustness checks
   varNames <- c("ppt", "tmax", "tmin")
   if( !varName %in% varNames) {
     stop("varName must be 'ppt', 'tmax', or 'tmin'.")
   }
-  if( !all(length(day) == 8) ) {
-    stop("day must be in yyyymmdd.")
+  if( dayStart > dayEnd ) {
+    stop("dayStart is after dayEnd")
   }
 
-  prism_fileName <- paste("PRISM", varName, "stable", "4kmD2", day, "bil", sep = "_")
-  prism::prism_set_dl_dir(path = dir)
-  if( !prism_fileName %in% prism::prism_archive_ls() ) {
-    stop("Raster does not exist in data_dir.")
+  # Loop over the range of dates to
+  # 1) Parse date into year, month and day
+  # 2) Create prism file name
+  fileNames <- character()
+  seqDates  <- seq(as.Date(dayStart), as.Date(dayEnd), by = "days")
+  for(day in seq_along(seqDates)) {
+    day <- seqDates[day]
+    #1)
+    yyyy <- lubridate::year(day)
+    mm   <- lubridate::month(day)
+    mm   <- stringr::str_pad(mm, 2, "0", side = "left")
+    dd   <- lubridate::day(day)
+    dd   <- stringr::str_pad(dd, 2, "0", side = "left")
+    dayN <- paste0(yyyy, mm, dd)
+    #2)
+    fileName  <- paste("PRISM", varName, "stable", "4kmD2", dayN, "bil", sep = "_")
+    fileNames <- c(fileNames, fileName)
   }
 
-  rast <- prism::pd_stack(prism_fileName)
+  # Check if the files exist in PRISM directory
+  prism::prism_set_dl_dir(path = data_dir)
+  if( !all(fileNames %in% prism::prism_archive_ls()) ) {
+    cat("\n")
+    cat("The following list of rasters do not exist in PRISM directory:")
+    cat("\n")
+    indices <- !(fileNames %in% prism::prism_archive_ls())
+    print(fileNames[indices])
+    stop("Raster does not exist in data_dir. Check above the list.")
+  }
 
-  polygonMeans <- exactextractr::exact_extract(rast,
+  raster_stack <- prism::pd_stack(fileNames)
+
+  polygonMeans <- exactextractr::exact_extract(raster_stack,
                                                shpFile,
                                                fun = 'mean')
 
